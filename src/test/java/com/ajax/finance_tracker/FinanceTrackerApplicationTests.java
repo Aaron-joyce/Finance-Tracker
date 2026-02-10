e97f8e0d6f3e48e7381ed7d49039e7e807fc6968
Aaron-joyce <aaronjoyce710@gmail.com>
2026-02-10 11:58:43 +0530

    restored email verification service

--- src/test/java/com/ajax/finance_tracker/FinanceTrackerApplicationTests.java
+++ src/test/java/com/ajax/finance_tracker/FinanceTrackerApplicationTests.java
@@ -1,7 +1,6 @@
 package com.ajax.finance_tracker;
 
 import com.ajax.finance_tracker.account.AccountService;
-import java.math.BigDecimal;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
